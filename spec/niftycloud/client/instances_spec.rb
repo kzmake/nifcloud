8594994a985205636e4b0985c9f2c60f13aa1de2
Kazuki Iwata <kazu.0516.k0n0f@gmail.com>
2017-10-15 21:27:55 +0900

    テスト: DescribeInstanceAttribute追加

--- spec/niftycloud/client/instances_spec.rb
+++ spec/niftycloud/client/instances_spec.rb
@@ -17,5 +17,22 @@ describe Nifcloud::Client do
         expect(@instances.reservationSet[0].instancesSet[0].instanceId).to eq("test")
       end
     end
+
+    context "Action: DescribeInstanceAttribute" do
+      before do
+        stub_get({Action: 'DescribeInstanceAttribute', InstanceId: 'test'})
+        @instance = Nifcloud.DescribeInstanceAttribute(InstanceId: 'test')
+      end
+
+      it "should get the correct resource" do
+        expect(a_get({Action: 'DescribeInstanceAttribute', InstanceId: 'test'})).to have_been_made
+      end
+
+      it "should return a response of instances" do
+        expect(@instance).to be_a Nifcloud::ObjectifiedHash
+        expect(@instance.instanceId).to eq("test")
+        expect(@instance.instanceUniqueId).to eq("i-06xhrmv7")
+      end
+    end
   end
 end
