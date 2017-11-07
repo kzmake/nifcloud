04a1728e60fa6011e605b5cc715dbc2c2b7971c5
Kazuki Iwata <kazu.0516.k0n0f@gmail.com>
2017-11-07 22:55:48 +0900

    add ConfigureHealthCheck method

--- lib/nifcloud/client/LoadBalancers.rb
+++ lib/nifcloud/client/LoadBalancers.rb
@@ -1,4 +1,8 @@
 class Nifcloud::Client
   module LoadBalancers
+    def ConfigureHealthCheck(options={})
+      options[:Action] = 'ConfigureHealthCheck'
+      get('/', query: options)
+    end
   end
 end
