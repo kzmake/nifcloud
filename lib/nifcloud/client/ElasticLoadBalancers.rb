20773a0875db259c1e60143a5c2875535c7c376e
Kazuki Iwata <kazu.0516.k0n0f@gmail.com>
2017-11-23 23:16:04 +0900

    add NiftyConfigureElasticLoadBalancerHealthCheck method

--- lib/nifcloud/client/ElasticLoadBalancers.rb
+++ lib/nifcloud/client/ElasticLoadBalancers.rb
@@ -1,4 +1,8 @@
 class Nifcloud::Client
   module ElasticLoadBalancers
+    def NiftyConfigureElasticLoadBalancerHealthCheck(options={})
+      options[:Action] = 'NiftyConfigureElasticLoadBalancerHealthCheck'
+      get('/', query: options)
+    end
   end
 end
